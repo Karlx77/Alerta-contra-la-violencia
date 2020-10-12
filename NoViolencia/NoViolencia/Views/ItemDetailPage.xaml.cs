0420342d00edc8a3623d10939c8189631df32fab
Karlx77 <katniss_12_ja@outlook.es>
2020-10-12 18:22:44 -0400

    CreandoAplicacionNoViolencia

---
+++ NoViolencia/NoViolencia/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NoViolencia.ViewModels;
+
+namespace NoViolencia.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ NoViolencia/NoViolencia/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NoViolencia.Models;
+using NoViolencia.ViewModels;
+
+namespace NoViolencia.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
